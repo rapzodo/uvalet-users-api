72f8edae9c7fc51ee41cb44450f62db597f9bb54
Z006LTL <R4ps0di@9>
2024-07-01 15:16:06 -0400

    migrating to openAI 1>

--- review.py
+++ review.py
@@ -6,29 +6,20 @@ from openai import OpenAI
 # Set up OpenAI API key
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
-github_token = os.getenv("GIT_TOKEN")
+github_token = os.getenv("TEMP_GIT_TOKEN")
 repo_full_name = os.getenv("GITHUB_REPOSITORY")
 
 
-# getting the repo and diff through gitHub api
-def fetch_pr_diff(repo, pr, token):
-    pr_diff_url = f"https://api.github.com/repos/{repo}/pulls/{pr}"
-    print(f"git info:{repo},{pr},{token}")
-    headers = {'Authorization': f'token {token}', 'Accept': 'application/vnd.github.v3.diff'}
-    diff_response = requests.get(pr_diff_url, headers=headers)
-    diff_response.raise_for_status()
-    return diff_response
+with open(os.getenv("GITHUB_EVENT_PATH")) as f:
+    event = json.load(f)
 
 
 # Read the event payload to get the PR diff
-with open(os.getenv("GITHUB_EVENT_PATH")) as f:
-    event = json.load(f)
-    print(f"event: {event}")
-
 pr_number = event['pull_request']['number']
 
-# Fetch the diff content from the URL
 diff_response = fetch_pr_diff(repo_full_name, pr_number, github_token)
+
+# Fetch the diff content from the URL
 print(f"diff response : {diff_response}")
 diff = diff_response.text
 print(f"diffs>{diff}")
@@ -40,7 +31,23 @@ response = client.chat.completions.create(
         {"role": "user", "content": f"Review the following code changes and provide comments:\n\n{diff}"}
     ]
 )
+comments = response.choices[0].message.content
 
 # Output the comments
-comments = response.choices[0].message.content
 print(comments)
+
+# getting the repo and diff through gitHub api
+def fetch_pr_diff(repo, pr, token):
+    pr_diff_url = f"https://api.github.com/repos/{repo}/pulls/{pr}"
+    print(f"git info:{repo},{pr},{token}")
+    headers = {'Authorization': f'token {token}', 'Accept': 'application/vnd.github.v3.diff'}
+    diff_response = requests.get(pr_diff_url, headers=headers)
+    diff_response.raise_for_status()
+    return diff_response
+
+
+
+
+
+
+
